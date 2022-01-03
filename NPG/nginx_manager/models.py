bfef1a5bbe64565c025915c927b59a405fc03c9e
Mia <yukana.miasu@gmail.com>
2022-01-03 15:46:18 +0100

    Made basic template manager model and integrated it in the admin panel
    TODO: 
    -Make it work (so it actually generate an nginx config)
    -Create a multiple template system for basic advanced and reverse proxy
    -Make edition to the user domains using cloudflare's API so it automatically manage the new config subdomain or DNS if needed

--- NPG/nginx_manager/models.py
+++ NPG/nginx_manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class ManagerConfig(models.Model):
+    server_listen_port = models.PositiveIntegerField()
+    server_name = models.TextField()
+    location_root = models.TextField()
+    location_index = models.TextField()
+
+
