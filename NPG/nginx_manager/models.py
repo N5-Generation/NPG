104aaa3ede4d8e31e62671ad9ef2d00f4fd9e523
Mia <yukana.miasu@gmail.com>
2022-01-03 16:33:35 +0100

    Added config name to Manager model + made the listing using the correct name

--- NPG/nginx_manager/models.py
+++ NPG/nginx_manager/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils import timezone 
 # Create your models here.
 
 class ManagerConfig(models.Model):
+    server_config_name = models.TextField(default="Untilted config [" + str(timezone.now) +"]")
     server_listen_port = models.PositiveIntegerField()
     server_name = models.TextField()
     location_root = models.TextField()
     location_index = models.TextField()
+
+    def __str__(self):
+        return self.server_config_name
 
 
