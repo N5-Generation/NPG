33d17135080e0faa289285673e37223039a9d678
Mia <yukana.miasu@gmail.com>
2022-01-26 14:42:13 +0100

    [Back] Edited nepgear to be compatible with django 4

--- N5/nepgear/urls.py
+++ N5/nepgear/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
@@ -8,6 +7,6 @@ app_name = 'nepgear'
 urlpatterns = [
     path("add-social", views.SocialCreationView.as_view(), name="social_creation"),
     path("social-admin", views.social_admin, name="social_admin"),
-    url(r'^delete/(?P<card>[0-9]+)/$', views.social_delete, name='social_delete'),
+    re_path(r'^delete/(?P<card>[0-9]+)/$', views.social_delete, name='social_delete'),
     path("", views.temp_dash, name="nepgear_dashboard"),
 ]
